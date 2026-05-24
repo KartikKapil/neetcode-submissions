b9db2e345956930af783371284abcc94b79b996f
Kartik Kapil <54640811+KartikKapil@users.noreply.github.com>
2026-05-24 21:50:08 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        map<char, int> mp;
+
+        for (char c : s) {
+            mp[c]++;
+        }
+
+        for (char c : t) {
+            if (mp.find(c) == mp.end()) return false;
+            mp[c]--;
+            if (mp[c] < 0) return false;
+        }
+
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            if (it->second != 0) return false;
+        }
+
+        return true;
+    }
+};
