b0cb282ace3194e5c0749be8a82e736c0a6a28e6
Kartik Kapil <54640811+KartikKapil@users.noreply.github.com>
2026-06-02 12:27:32 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    int characterReplacement(string s, int k) {
+        int i = 0;
+        int j = 0;
+        int maxElementCount = 0;
+        // int maxElement = s[0];
+        int maxLen = 0;
+        map<char, int> mp;
+        while (j < s.length()) {
+            mp[s[j]]++;
+            maxElementCount = max(maxElementCount, mp[s[j]]);
+            if(j-i+1-maxElementCount<=k) {
+                maxLen=max(maxLen, j-i+1);
+            } else {
+                while(i<j&& j-i+1-maxElementCount > k) {
+                    mp[s[i]]--;
+                    maxElementCount=0;
+                    for(auto itr:mp) {
+                        maxElementCount = max(maxElementCount, itr.second);
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return maxLen;
+    }
+};
